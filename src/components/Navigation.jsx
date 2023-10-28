c8d7331fe314524b7c5061525859af3f1f19de58
Yusuf Abubeker <yusufabubeker2@gmail.com>
2023-10-28 06:46:20 -0700

    link update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/Navigation.css'
+import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <nav className="nav">
       <ul className="nav-list">
         <li className="nav-item"><a href="/">Home</a></li>
-        <li className="nav-item"><a href="/books">Book List</a></li>
+        <li className="nav-item"><Link to="/books">Book List</Link></li>
         <li className="nav-item"><a href="/about">About</a></li>
         <li className="nav-item"><a href="/contact">Contact</a></li>
       </ul>
