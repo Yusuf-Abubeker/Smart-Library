92711ddfe3ae02b90f5bf3cf97b0849c418401fb
Yusuf Abubeker <yusufabubeker2@gmail.com>
2023-10-28 04:13:25 -0700

    modification on app module

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/books" element={<BookList/>}/>
-        <Route path="*" element={<ErrorPage/>}/>
+        <Route path="/*" element={<ErrorPage/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
